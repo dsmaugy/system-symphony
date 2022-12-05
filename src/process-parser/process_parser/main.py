3c7863bac0ba97cdf6ae3f1f9f607248a0b1f85e
Darwin <darwin78913@gmail.com>
2022-12-05 17:16:53 -0500

    first push with python code

---
+++ src/process-parser/process_parser/main.py
@@ -0,0 +1,41 @@
+
+import os
+from time import sleep
+from typing import Dict
+from pythonosc import udp_client  
+
+import psutil
+
+PROCESS_IGNORE_LIST = {"root"}
+CURRENT_PID = os.getpid()
+
+"""
+Potentially Interested in these numerical values:
+'cpu_percent', 'cpu_times', 'memory_percent', 'nice', 'num_ctx_switches', 'num_fds', 'num_threads'
+
+cpu_times -> kernel/user
+"""
+
+def valid_process(p_info: Dict) -> bool:
+    return p_info["username"] not in PROCESS_IGNORE_LIST and p_info["pid"] != CURRENT_PID
+
+def poll_processes(poll_rate: int=1000):
+    while True:
+        processes = list(psutil.process_iter())
+        cleaned_processes = {}
+
+        for p in processes:
+            p_info = p.as_dict()
+            if valid_process(p_info):
+                print(f"User: {p_info['username']} Application: {p_info['name']}")
+                if p_info['name'] in cleaned_processes:
+                    pass
+                else:
+                    cleaned_processes[p_info['name']] = {"cpu"}
+                
+
+        sleep(poll_rate/1000)
+        
+
+if __name__ == "__main__":
+    poll_processes()
